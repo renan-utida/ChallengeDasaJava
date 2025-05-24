56d49c59b6d0eb97f8c31647fd1d809cd1c4854b
Renan <renandiutida@gmail.com>
2025-05-24 00:33:15 -0300

    TecnicoLaboratorio.java: classe criada para o responsavel por tirar os insumos, alem de atributo como crbm e o metodo de apresentar

---
+++ src/main/java/dasa/funcionarios/TecnicoLaboratorio.java
@@ -0,0 +1,41 @@
+package dasa.funcionarios;
+
+/**
+ * Classe específica para Técnicos em Laboratório
+ */
+public class TecnicoLaboratorio extends Funcionario {
+    private int crbm;
+
+    // Construtor padrão
+    public TecnicoLaboratorio() {
+        super();
+    }
+
+    // Construtor com parâmetros
+    public TecnicoLaboratorio(String nome, int crbm) {
+        super(nome, String.valueOf(crbm));
+        this.crbm = crbm;
+    }
+
+    // Getter e Setter específicos
+    public int getCrbm() {
+        return crbm;
+    }
+
+    public void setCrbm(int crbm) {
+        this.crbm = crbm;
+        this.registro = String.valueOf(crbm); // Mantém sincronizado com a classe pai
+    }
+
+    // Sobrescrita do metodo apresentar (Polimorfismo)
+    @Override
+    public void apresentar() {
+        System.out.println("CRBM: " + crbm);
+        System.out.println("\tNome do(a) Técnico(a): " + nome);
+    }
+
+    // Metodo específico do técnico
+    public void acessarSistema() {
+        System.out.println("Olá, " + nome + ", seja bem vindo ao nosso sistema!");
+    }
+}
